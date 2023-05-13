3337da3e962bd47826cd6924a3ca884d38c3bb59
umar-bin-khalid <umar.ubk@gmail.com>
2023-05-13 18:07:28 +0500

    Add tests for tac factory.

--- farm_project/api/models/tests/Tac.py
+++ farm_project/api/models/tests/Tac.py
@@ -1,7 +1,6 @@
 from django.test import TestCase
 from api.models import Tac 
- 
-
+from api.factories import TacFactory
 
 
 class TacTestCase(TestCase):
@@ -14,4 +13,17 @@ class TacTestCase(TestCase):
         self.assertEquals(
             Tac.objects.count(),
             1
-        ) 
+        )
+        # create a TAC object with default attributes
+        tac = TacFactory.create()
+        # assert that the object was created successfully
+        self.assertIsInstance(tac, Tac)
+        self.assertIsNotNone(tac.code)
+        self.assertIsNotNone(tac.insert_user_id)
+        self.assertIsNotNone(tac.last_update_user_id)
+        self.assertIsNotNone(tac.description)
+        self.assertIsNotNone(tac.display_order)
+        self.assertIsNotNone(tac.is_active)
+        self.assertIsNotNone(tac.lookup_enum_name)
+        self.assertIsNotNone(tac.name)
+        self.assertIsNotNone(tac.pac)
