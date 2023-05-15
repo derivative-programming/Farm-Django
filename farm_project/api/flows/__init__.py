f1dd7d322ad30ecd386068ff8076f140cf468c84
Vince Roche <vince.roche@gmail.com>
2023-05-15 10:00:19 -0400

    template gen updates

--- farm_project/api/flows/__init__.py
+++ farm_project/api/flows/__init__.py
@@ -1,6 +1,19 @@
+from .base.flow_validation_error import FlowValidationError
+##GENLOOPObjectStart
+##GENTrainingBlock[a]Start
+##GENLearn[modelType=object,name=Land]Start 
+##GENLOOPObjectWorkflowStart
+##GENTrainingBlock[b]Start
+##GENLearn[modelType=objectWorkflow,name=LandAddPlant]Start 
+from .land_add_plant import FlowLandAddPlant,FlowLandAddPlantResult
+##GENLearn[modelType=objectWorkflow,name=LandAddPlant]End
+##GENTrainingBlock[b]End
+##GENLOOPObjectWorkflowEnd
+##GENLearn[modelType=object,name=Land]End 
 from .tac_farm_dashboard_init_report import FlowTacFarmDashboardInitReport, FlowTacFarmDashboardInitReportResult 
 from .tac_login import FlowTacLogin, FlowTacLoginResult 
 from .tac_login_init_obj_wf import FlowTacLoginInitObjWF, FlowTacLoginInitObjWFResult 
 from .tac_register import FlowTacRegister, FlowTacRegisterResult 
 from .tac_register_init_obj_wf import FlowTacRegisterInitObjWF, FlowTacRegisterInitObjWFResult 
-from .base.flow_validation_error import FlowValidationError
+##GENTrainingBlock[a]End
+##GENLOOPObjectEnd
--- farm_project/api/flows/base/__init__.py
+++ farm_project/api/flows/base/__init__.py
@@ -1,10 +1,21 @@
-
 from .base_flow import BaseFlow
 from .log_severity import LogSeverity
 from .flow_validation_error import FlowValidationError
+##GENLOOPObjectStart
+##GENTrainingBlock[a]Start
+##GENLearn[modelType=object,name=Land]Start 
+##GENLOOPObjectWorkflowStart
+##GENTrainingBlock[b]Start
+##GENLearn[modelType=objectWorkflow,name=LandAddPlant]Start 
+from .land_add_plant import BaseFlowLandAddPlant
+##GENLearn[modelType=objectWorkflow,name=LandAddPlant]End
+##GENTrainingBlock[b]End
+##GENLOOPObjectWorkflowEnd
+##GENLearn[modelType=object,name=Land]End 
 from .tac_register_init_obj_wf import BaseFlowTacRegisterInitObjWF
 from .tac_login_init_obj_wf import BaseFlowTacLoginInitObjWF
 from .tac_farm_dashboard_init_report import BaseFlowTacFarmDashboardInitReport
 from .tac_register import BaseFlowTacRegister
 from .tac_login import BaseFlowTacLogin
-from .land_add_plant import BaseFlowLandAddPlant
+##GENTrainingBlock[a]End
+##GENLOOPObjectEnd
